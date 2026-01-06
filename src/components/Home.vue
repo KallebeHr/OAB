ef718cc67d495a055d52834bde3bf9918245e2e9
KallebeHr <kallebealmeidap2@gmail.com>
2026-01-06 16:10:33 -0300

    Mauricio

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -48,7 +48,7 @@
       </div>
       <div class="cta-wrapper anim-item delay-5">
         <button class="btn-cta">
-          Fale com ADVOGADO 
+          Fale com um ADVOGADO 
           <i class="fa-brands fa-whatsapp ml-2"></i>
         </button>
       </div>
--- src/components/PreLoader.vue
+++ src/components/PreLoader.vue
@@ -1,10 +1,10 @@
 <template>
   <div class='container'>
 <div class="textWrapper">
-  <p class="text">Dr. <span class="highlight">Seu Nome</span></p>
+  <p class="text">Dr. <span class="highlight">Mauricio</span></p>
   <div class="invertbox"></div>
 </div>
-<img src="/LogoPNG.png" class="logoName" alt="">
+<img src="/LogoPNG4.png" class="logoName" alt="">
 </div>
 </template>
 
