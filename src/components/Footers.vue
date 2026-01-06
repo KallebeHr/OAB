adcb1a9796a336aa40e45ca0d969b0f6d39cd81b
KallebeHr <kallebealmeidap2@gmail.com>
2026-01-06 01:37:18 -0300

    Teste

--- src/components/Footers.vue
+++ src/components/Footers.vue
@@ -5,7 +5,7 @@
         
         <!-- Coluna Esquerda: Logo e Redes Sociais -->
         <div class="footer-left">
-          <img  src="/LogoPNG2.png"  alt="" class="logo">
+          <img  src="/LogoPNG.png"  alt="" class="logo">
           <p class="description">
             Soluções jurídicas personalizadas, com compromisso, ética e excelência para alcançar o melhor resultado.
           </p>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -48,7 +48,7 @@
       </div>
       <div class="cta-wrapper anim-item delay-5">
         <button class="btn-cta">
-          Fale com Dr. Breno Martins 
+          Fale com ADVOGADO 
           <i class="fa-brands fa-whatsapp ml-2"></i>
         </button>
       </div>
